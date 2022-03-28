593a6ff9fe21c97295cb9bd1f5aed96281d9fd3a
mohamilin <amilinmoh99@gmail.com>
2022-03-28 20:49:30 +0700

    register middleware admin and mentor in kernel

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -63,5 +63,9 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+
+        // Middleware
+        'admin' => \App\Http\Middleware\IsAdmin::class,
+        'mentor' => \App\Http\Middleware\IsMentor::class,
     ];
 }
