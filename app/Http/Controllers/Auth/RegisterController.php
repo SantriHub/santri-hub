bb758a0e3c70312d5a7ff97280223bd30e118e4e
mohamilin <amilinmoh99@gmail.com>
2022-03-28 20:50:23 +0700

    refactor for login create

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -6,8 +6,8 @@ use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use App\Models\User;
 use Illuminate\Foundation\Auth\RegistersUsers;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Validator;
 
 class RegisterController extends Controller
 {
@@ -23,6 +23,11 @@ class RegisterController extends Controller
     */
 
     use RegistersUsers;
+
+    public function index()
+    {
+        return view('auth.register');
+    }
 
     /**
      * Where to redirect users after registration.
@@ -47,27 +52,24 @@ class RegisterController extends Controller
      * @param  array  $data
      * @return \Illuminate\Contracts\Validation\Validator
      */
-    protected function validator(array $data)
-    {
-        return Validator::make($data, [
-            'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'password' => ['required', 'string', 'min:8', 'confirmed'],
-        ]);
-    }
 
     /**
-     * Create a new user instance after a valid registration.
+     * Create a new user instance with a validaton.
      *
-     * @param  array  $data
+     * @param @request
      * @return \App\Models\User
      */
-    protected function create(array $data)
+    protected function create(Request $request)
     {
-        return User::create([
-            'name' => $data['name'],
-            'email' => $data['email'],
-            'password' => Hash::make($data['password']),
+        $validateData = $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'password' => ['required', 'string', 'min:8'],
         ]);
+
+        $validateData['password'] = hash::make($validateData['password']);
+
+        User::create($validateData);
+        return redirect('/login')->with('success', 'Register Sukses, Cek email untuk verifikasi');
     }
 }
