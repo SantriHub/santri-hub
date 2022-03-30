8e6517c4a8aca915e65a4a60042490e1b6bc88b9
mohamilin <amilinmoh99@gmail.com>
2022-03-30 10:59:21 +0700

    url -> asset

--- resources/views/includes/script.blade.php
+++ resources/views/includes/script.blade.php
@@ -1,10 +1,10 @@
     <!-- Bootstrap JS -->
-    <script src="{{ url('template/assets/vendor/bootstrap/dist/js/bootstrap.bundle.min.js')}}"></script>
+    <script src="{{ asset('template/assets/vendor/bootstrap/dist/js/bootstrap.bundle.min.js')}}"></script>
 
     <!-- Vendors -->
-    <script src="{{ url('template/assets/vendor/tiny-slider/tiny-slider.js')}}"></script>
-    <script src="{{ url('template/assets/vendor/glightbox/js/glightbox.js')}}"></script>
-    <script src="{{ url('template/assets/vendor/purecounterjs/dist/purecounter_vanilla.js')}}"></script>
-    <script src="{{ url('template/assets/vendor/apexcharts/js/apexcharts.min.js')}}"></script>
+    <script src="{{ asset('template/assets/vendor/tiny-slider/tiny-slider.js')}}"></script>
+    <script src="{{ asset('template/assets/vendor/glightbox/js/glightbox.js')}}"></script>
+    <script src="{{ asset('template/assets/vendor/purecounterjs/dist/purecounter_vanilla.js')}}"></script>
+    <script src="{{ asset('template/assets/vendor/apexcharts/js/apexcharts.min.js')}}"></script>
     <!-- Template Functions -->
-    <script src="{{ url('template/assets/js/functions.js')}}"></script>
+    <script src="{{ asset('template/assets/js/functions.js')}}"></script>
