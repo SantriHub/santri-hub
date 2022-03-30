6cb0b03702648065ebe81d7bf9ba46241e63a908
mohamilin <amilinmoh99@gmail.com>
2022-03-30 19:02:21 +0700

    secure_asset

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -168,7 +168,16 @@
 <!-- **************** MAIN CONTENT END **************** -->
 
 <!-- Bootstrap JS -->
-@include('includes.script')
+{{-- @include('includes.script') --}}
+    <!-- Bootstrap JS -->
+    <script src="{{ secure_asset('template/assets/vendor/bootstrap/dist/js/bootstrap.bundle.min.js')}}"></script>
 
+    <!-- Vendors -->
+    <script src="{{ secure_asset('template/assets/vendor/tiny-slider/tiny-slider.js')}}"></script>
+    <script src="{{ secure_asset('template/assets/vendor/glightbox/js/glightbox.js')}}"></script>
+    <script src="{{ secure_asset('template/assets/vendor/purecounterjs/dist/purecounter_vanilla.js')}}"></script>
+    <script src="{{ secure_asset('template/assets/vendor/apexcharts/js/apexcharts.min.js')}}"></script>
+    <!-- Template Functions -->
+    <script src="{{ secure_asset('template/assets/js/functions.js')}}"></script>
 </body>
 </html>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -12,7 +12,20 @@
 	<!-- Favicon -->
 	<link rel="shortcut icon" href="assets/images/favicon.ico">
 
-    @include('includes.style')
+	 <!-- Google Font -->
+	 <link rel="preconnect" href="https://fonts.googleapis.com">
+	 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	 <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Heebo:wght@400;500;700&family=Roboto:wght@400;500;700&display=swap">
+	
+	 <!-- Plugins CSS -->
+	 <link rel="stylesheet" type="text/css" href="{{ secure_asset('template/assets/vendor/font-awesome/css/all.min.css')}}">
+	 <link rel="stylesheet" type="text/css" href="{{ secure_asset('template/assets/vendor/bootstrap-icons/bootstrap-icons.css')}}">
+	 <link rel="stylesheet" type="text/css" href="{{ secure_asset('template/assets/vendor/tiny-slider/tiny-slider.css')}}">
+	 <link rel="stylesheet" type="text/css" href="{{ secure_asset('template/assets/vendor/glightbox/css/glightbox.css')}}">
+	 <link rel="stylesheet" type="text/css" href="{{ secure_asset('template/assets/vendor/apexcharts/css/apexcharts.css')}}">
+	
+	 <!-- Theme CSS -->
+	 <link id="style-switch" rel="stylesheet" type="text/css" href="{{ secure_asset('template/assets/css/style.css')}}">
 </head>
 
 <body>
@@ -153,7 +166,15 @@
 <!-- **************** MAIN CONTENT END **************** -->
 
 <!-- Back to top -->
-@include('includes.script')
+<script src="{{ secure_asset('template/assets/vendor/bootstrap/dist/js/bootstrap.bundle.min.js')}}"></script>
+
+<!-- Vendors -->
+<script src="{{ secure_asset('template/assets/vendor/tiny-slider/tiny-slider.js')}}"></script>
+<script src="{{ secure_asset('template/assets/vendor/glightbox/js/glightbox.js')}}"></script>
+<script src="{{ secure_asset('template/assets/vendor/purecounterjs/dist/purecounter_vanilla.js')}}"></script>
+<script src="{{ secure_asset('template/assets/vendor/apexcharts/js/apexcharts.min.js')}}"></script>
+<!-- Template Functions -->
+<script src="{{ secure_asset('template/assets/js/functions.js')}}"></script>
 
 </body>
 </html>
