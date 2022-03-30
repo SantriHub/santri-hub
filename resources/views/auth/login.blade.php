40cca33636c9d63c43b601f7a55ea05f50f5954b
mohamilin <amilinmoh99@gmail.com>
2022-03-30 18:58:20 +0700

    secure_asset

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -20,14 +20,14 @@
 	 <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Heebo:wght@400;500;700&family=Roboto:wght@400;500;700&display=swap">
 	
 	 <!-- Plugins CSS -->
-	 <link rel="stylesheet" type="text/css" href="{{ asset('template/assets/vendor/font-awesome/css/all.min.css')}}">
-	 <link rel="stylesheet" type="text/css" href="{{ asset('template/assets/vendor/bootstrap-icons/bootstrap-icons.css')}}">
-	 <link rel="stylesheet" type="text/css" href="{{ asset('template/assets/vendor/tiny-slider/tiny-slider.css')}}">
-	 <link rel="stylesheet" type="text/css" href="{{ asset('template/assets/vendor/glightbox/css/glightbox.css')}}">
-	 <link rel="stylesheet" type="text/css" href="{{ asset('template/assets/vendor/apexcharts/css/apexcharts.css')}}">
+	 <link rel="stylesheet" type="text/css" href="{{ secure_asset('template/assets/vendor/font-awesome/css/all.min.css')}}">
+	 <link rel="stylesheet" type="text/css" href="{{ secure_asset('template/assets/vendor/bootstrap-icons/bootstrap-icons.css')}}">
+	 <link rel="stylesheet" type="text/css" href="{{ secure_asset('template/assets/vendor/tiny-slider/tiny-slider.css')}}">
+	 <link rel="stylesheet" type="text/css" href="{{ secure_asset('template/assets/vendor/glightbox/css/glightbox.css')}}">
+	 <link rel="stylesheet" type="text/css" href="{{ secure_asset('template/assets/vendor/apexcharts/css/apexcharts.css')}}">
 	
 	 <!-- Theme CSS -->
-	 <link id="style-switch" rel="stylesheet" type="text/css" href="{{ asset('template/assets/css/style.css')}}">
+	 <link id="style-switch" rel="stylesheet" type="text/css" href="{{ secure_asset('template/assets/css/style.css')}}">
 
 </head>
 
