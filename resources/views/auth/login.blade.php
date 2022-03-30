7989f867c571e028ab8ec0b52ff3aad70b25e5ec
mohamilin <amilinmoh99@gmail.com>
2022-03-30 18:43:42 +0700

    reload css

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -13,7 +13,21 @@
 	<link rel="shortcut icon" href="assets/images/favicon.ico">
 
 	<!-- Google Font -->
-    @include('includes.style')
+    {{-- @include('includes.style') --}}
+	 <!-- Google Font -->
+	 <link rel="preconnect" href="https://fonts.googleapis.com">
+	 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	 <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Heebo:wght@400;500;700&family=Roboto:wght@400;500;700&display=swap">
+	
+	 <!-- Plugins CSS -->
+	 <link rel="stylesheet" type="text/css" href="{{ asset('template/assets/vendor/font-awesome/css/all.min.css')}}">
+	 <link rel="stylesheet" type="text/css" href="{{ asset('template/assets/vendor/bootstrap-icons/bootstrap-icons.css')}}">
+	 <link rel="stylesheet" type="text/css" href="{{ asset('template/assets/vendor/tiny-slider/tiny-slider.css')}}">
+	 <link rel="stylesheet" type="text/css" href="{{ asset('template/assets/vendor/glightbox/css/glightbox.css')}}">
+	 <link rel="stylesheet" type="text/css" href="{{ asset('template/assets/vendor/apexcharts/css/apexcharts.css')}}">
+	
+	 <!-- Theme CSS -->
+	 <link id="style-switch" rel="stylesheet" type="text/css" href="{{ asset('template/assets/css/style.css')}}">
 
 </head>
 
