6a1bc14a22270a874d8071b6351979fe9aea8246
mohamilin <amilinmoh99@gmail.com>
2022-04-03 13:21:27 +0700

    refactor routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,11 @@
 <?php
 
+use App\Http\Controllers\Admin\CityController;
+use App\Http\Controllers\Admin\CollegeController;
+use App\Http\Controllers\Admin\CountryController;
+use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\Admin\MentorController;
+use App\Http\Controllers\Admin\StateController;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\HomeController;
@@ -39,19 +45,55 @@ Route::get('/kampus/itb/ahmad-fulan', function () {
     return view('pages.mentor.detail-mentor');
 });
 
-Route::get('/dashboard/mentor', function () {
-    return view('pages.mentor.dashboard-mentor');
-})->middleware(['auth']);
+// Route::get('/dashboard/mentor', function () {
+//     return view('pages.mentor.dashboard-mentor');
+// })->middleware(['auth', 'mentor']);
 
 
 /**
  * Member
  */
 
-Route::get('/dashboard/member', function () {
-    return view('pages.member.dashboard-member');
-})->middleware(['auth']);
+// Route::get('/dashboard/member', function () {
+//     return view('pages.member.dashboard-member');
+// })->middleware(['auth', 'member']);
 
 /**
  * Admin
  */
+
+
+// Route::get('/dashboard/admin', function () {
+//     return view('pages.admin.dashboard');
+// });
+
+Route::prefix('admin')
+    ->middleware(['auth', 'admin'])
+    ->group(function () {
+        Route::get('/', [DashboardController::class, 'index'])
+            ->name('dashboard');
+        Route::resource('countries', CountryController::class);
+        Route::resource('states', StateController::class);
+        Route::resource('cities', CityController::class);
+        Route::resource('colleges', CollegeController::class);
+        Route::resource('mentors', MentorController::class);
+    });
+
+// Route::prefix('countries')
+//     ->namespace('Admin')
+//     ->group(function () {
+//         Route::get('/', [CountryController::class, 'index'])->name('countries');
+//     });
+
+// Route::get('/states', function () {
+//     return view('pages.admin.states.index');
+// });
+
+
+// Route::get('/cities', function () {
+//     return view('pages.admin.cities.index');
+// });
+
+// Route::get('/colleges', function () {
+//     return view('pages.admin.colleges.index');
+// });
