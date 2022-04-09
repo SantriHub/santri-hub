14dcdc3e71aaabd709c26e260cda2d0a5cc39045
mohamilin <amilinmoh99@gmail.com>
2022-04-09 19:42:14 +0700

    mentor controller

--- app/Http/Controllers/Admin/MentorController.php
+++ app/Http/Controllers/Admin/MentorController.php
@@ -3,17 +3,36 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Admin\MentorRequest;
+use App\Models\College;
+use App\Models\Mentor;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class MentorController extends Controller
 {
     public function index()
     {
-        return view('pages.admin.mentors.index');
+        $data = Mentor::all();
+
+        return view('pages.admin.mentors.index', ["items" => $data]);
     }
 
     public function create()
     {
-        return view('pages.admin.mentors.create');
+        $dataUser = User::all();
+        $dataCollege = College::all();
+        return view('pages.admin.mentors.create', ["users" => $dataUser, "colleges" => $dataCollege]);
+    }
+
+    public function store(MentorRequest $request)
+    {
+        $data = $request->all();
+        $dataMentor = Mentor::find($data['user_id']);
+        if ($dataMentor) {
+            return redirect()->route('mentors.index')->with('error', 'Mentor name was used');
+        }
+        Mentor::create($data);
+        return redirect()->route('mentors.index')->with('success', 'Add a Mentor Success');
     }
 }
