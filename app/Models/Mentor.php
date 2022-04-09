da4b0ec7e60ea008e786330d22811795fbd5737a
mohamilin <amilinmoh99@gmail.com>
2022-04-09 19:44:02 +0700

    mentor model

--- app/Models/Mentor.php
+++ app/Models/Mentor.php
@@ -4,8 +4,29 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Mentor extends Model
 {
-    use HasFactory;
+    // use HasFactory;
+    use SoftDeletes;
+    protected $fillable = [
+        "mentor_name",
+        "user_id",
+        "college_id",
+        "short_description",
+        "active"
+    ];
+
+    protected $hidden = [];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function college()
+    {
+        return $this->belongsTo(College::class);
+    }
 }
